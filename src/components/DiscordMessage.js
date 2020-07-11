8181c3489a2d86934e56bb54ddd4b630441f86f5
Arthur De Witte <arthur.dewitte@gmail.com>
2020-07-11 23:19:49 +0200

    Optimization (Removal of unnecessary span element)

--- src/components/DiscordMessage.js
+++ src/components/DiscordMessage.js
@@ -217,12 +217,12 @@ function parseData(message, type) {
           case "(":
             if (message[index + 2] === ")") {
               data[index - 1] = (
-                <span key={index}>
+                <React.Fragment key={index}>
                   <span key={index} className="lightblue">
                     {message[index - 1]}
                   </span>
                   {"("}
-                </span>
+                </React.Fragment>
               );
             }
             break;
